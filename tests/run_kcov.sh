ecb04a026de8c7fee892ef75ee50a63719bf2a99
Tom <TommyE123@users.noreply.github.com>
2018-06-26 06:34:36 +0100

    Update run_kcov.sh

--- tests/run_kcov.sh
+++ tests/run_kcov.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-sudo bash ./main.sh -t validate_newline
+source "./tests/validate_newline.sh"
+validate_newline
 sudo bash ./main.sh -t validate_bashate
 sudo bash ./main.sh -t validate_shellcheck
 sudo bash ./main.sh -t run_install
