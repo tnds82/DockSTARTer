e8ce79352716d1e49b51f53284f51ee5eeb26caa
Tom <TommyE123@users.noreply.github.com>
2018-06-26 11:16:07 +0100

    Update run_kcov.sh

--- tests/run_kcov.sh
+++ tests/run_kcov.sh
@@ -5,16 +5,16 @@ IFS=$'\n\t'
 source "./tests/validate_newline.sh"
 validate_newline
 
-source "./tests/validate_bashate.sh"
-validate_bashate
+# source "./tests/validate_bashate.sh"
+# validate_bashate
 
-source "./tests/validate_shellcheck.sh"
-validate_shellcheck
+# source "./tests/validate_shellcheck.sh"
+# validate_shellcheck
 
-source "./tests/run_install.sh"
-run_install
+# source "./tests/run_install.sh"
+# run_install
 
-source "./tests/run_generate.sh"
-run_generate
+# source "./tests/run_generate.sh"
+# run_generate
 
 echo "Finished"
